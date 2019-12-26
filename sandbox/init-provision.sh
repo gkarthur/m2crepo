8c21e5dff32988d51343edb9b23b1145fd8f0b5c
Kouatelay Gahie <gkarthur@gmail.com>
2019-12-26 22:23:47 +0100

    deploy ansible

--- sandbox/init-provision.sh
+++ sandbox/init-provision.sh
@@ -4,6 +4,11 @@ sudo yum install wget -y
 sudo yum install curl -y
 sudo yum install unzip -y
 sudo yum install ansible -y
+sudo yum install epel-release -y
+sudo yum install python-pip -y
+sudo pip install boto
+sudo pip install boto3
+
 curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
